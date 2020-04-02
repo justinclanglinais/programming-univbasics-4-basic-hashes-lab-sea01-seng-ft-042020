18c478e2c5802ea34bb9e9799b29a864a0909949
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-02 03:42:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  dogs = {
+    "Lucy" => "miniature schnauzer",
+    "Cosmo" => "Samoyed",
+    "Griffin" => "Terrier"
+  }
 end
 
 def pioneer
